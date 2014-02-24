b403ab6cf9d200bb90c3cabd7c3aec6ac7a2cf6c
baris.ozkan <baris.ozkan@erkyazilim.com.tr>
2014-02-24 08:52:11 +0200

    first run.

---
+++ BootstrapSelect/BootstrapSelect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapSelect.Startup))]
+namespace BootstrapSelect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
