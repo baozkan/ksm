1744e4b1963862b586b4d7f0faa8f8d6598ca948
baris.ozkan <baris.ozkan@erkyazilim.com.tr>
2014-03-13 11:54:07 +0200

    Add order.
    Add order.

---
+++ FooInterface/FooInterface/Foo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooInterface
+{
+    class Foo : IFoo
+    {
+        //imp.
+        public void Bar()
+        {
+            Console.WriteLine("I am bar.");
+        }
+
+        //Exp.
+        void IFoo.Foo()
+        {
+            Console.WriteLine("I am foo.");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("Hello world.");
+        }
+    }
+}
